7af41c1c7e10ca81eb458c40ed75a8de79ee5876
David Hambraeus <ddhambraeus@gmail.com>
2019-05-28 14:59:31 +0200

    Lade till metod för att finna om en graf är sammanhängande som tar en dictionary så att vi inte behöver oroa oss för nod idn

--- source/DepthFirst.cs
+++ source/DepthFirst.cs
@@ -2,11 +2,18 @@
 
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 public class DFS{
     public static void Main(){
         //int[][] architecture = new int[][] {new int[] {1,2},new int[] {0},new int[] {0}};
-        int[][] architecture = new int[][] {new int[] {1,2},new int[] {0},new int[] {0,4},new int[] {4}, new int[] {3,2}};
+        //int[][] architecture = new int[][] {new int[] {1,2},new int[] {0},new int[] {0,4},new int[] {4}, new int[] {3,2}};
+        var architecture = new Dictionary<int,int[]>();
+        architecture.Add(5,new int[] {10,100});
+        architecture.Add(10,new int[] {5});
+        architecture.Add(100,new int[] {5});
+        architecture.Add(2,new int[] {});
+
 
         if (IsConnected(architecture)){
             Console.WriteLine("Ye mate");
@@ -15,6 +22,42 @@ public class DFS{
             Console.WriteLine("Na mate");
         }
 
+    }
+    public static bool IsConnected(Dictionary<int,int[]> graph){
+        int n = graph.Count;
+
+        // Alla element i seen borde vara false utom första. 
+        var seen = new Dictionary<int,bool>();
+        var visited = new Dictionary<int,bool>();
+        foreach (int key in graph.Keys){
+            seen.Add(key,false);
+            visited.Add(key,false);
+        }
+
+        int currentNode = graph.Keys.FirstOrDefault();
+        seen[currentNode] = true;
+        while (true){
+            visited[currentNode] = true;
+
+            foreach (int ID in graph[currentNode]){
+                seen[ID] = true;
+            }
+            bool found = false;
+            //for (int nextNode = 1; nextNode < n; nextNode++){
+            foreach (int nextNode in seen.Keys){
+                if (seen[nextNode] && !visited[nextNode]){
+                    currentNode = nextNode;
+                    found = true;
+                    break;
+                }
+            }
+            if (seen.Values.All(x=>x)){
+                return true;
+            }
+            if (!found){
+                return false;
+            }
+        }
     }
     public static bool IsConnected(int[][] graph){
         int n = graph.Length;
