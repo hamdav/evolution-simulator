002e8a00d37f7603d15e350d376fcc5f2548dc55
David Hambraeus <ddhambraeus@gmail.com>
2019-06-15 15:26:41 +0200

    added id as argument

--- source/Node.cs
+++ source/Node.cs
@@ -6,13 +6,13 @@ namespace Creature
 {
     public class Node
     {
-        public Node(Node n)
+        public Node(Node n, int id)
         {
             this.position = 0;
             this.velocity = 0;
             this.acceleration = 0;
             this.force = 0;
-            this.id = //TODO lista ut hur vi ska göra med id...
+            this.id = id;
 
             Random rand = new Random();
             double mutationFactor = 0.1; 
@@ -23,14 +23,14 @@ namespace Creature
             double dmass = rand.NextDouble()*mutationFactor - mutationFactor/2;
             this.mass = n.mass + dmass;
         }
-        public Node(){
+        public Node(int id){
             Random rand = new Random();
             
             this.position = 0;
             this.velocity = 0;
             this.acceleration = 0;
             this.force = 0;
-            this.id = //TODO lista ut hur vi ska göra med id...
+            this.id = id;
             this.mu = rand.NextDouble();
             this.mass = rand.NextDouble();
         }
