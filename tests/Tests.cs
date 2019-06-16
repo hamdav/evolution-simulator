8aea3b9a666a9b87b6c3975f1cc6a7f3de7bc86b
David Hambraeus <ddhambraeus@gmail.com>
2019-06-16 15:28:19 +0200

    Added test

---
+++ tests/Tests.cs
@@ -0,0 +1,10 @@
+// Tests of Node and Muscle
+using System;
+using Creature;
+public class Tests{
+    public static void Main(){
+        // Test of Node
+        Node n1 = new Node(1);
+        Node n2 = new Node(n1,2);
+    }
+}
